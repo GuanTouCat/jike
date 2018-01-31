193b7504aeaa00e406e5aee59b33cacd617faf8a
“GuanTouCat” <ji997270186@gmail.com>
2018-01-31 15:12:07 +0800

    sb了

--- common/js/nav.js
+++ common/js/nav.js
@@ -1,3 +1,8 @@
 /**
  * Created by lansetudou on 2018/1/31.
  */
+$(function () {
+   var index = window.location.href.split("/").length-1;
+   var hre = window.location.href.split("/")[index];
+   $(".list li a[href='"+hre+"']").addClass("on");
+});
